837592f524e27c75b7f0d69854e5fb10706dab11
rsmsnot <rsmsnot@outlook.com>
2019-06-28 03:19:48 +0000

    complete

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let total = 0;
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+       total += sumItems(item);
+    } else {
+       total += item
+    }
+  }) 
+  return total;
 }
 
 module.exports = sumItems;
